825d2abb77b8e2e57168436a6c8f72db6e5f491d
Davi Mendes <mendes08.paixao@gmail.com>
2026-04-10 13:21:17 -0300

    Enhance scrollToSection with menu toggle and observer
    Added toggleMenu function and IntersectionObserver for sections.

--- anime.js
+++ anime.js
@@ -3,3 +3,25 @@ function scrollToSection(id) {
                 behavior: 'smooth'
                 });
             }
+function toggleMenu() {
+            const menu = document.getElementById('menu');
+            menu.classList.toggle('active');
+            }
+            
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+sections.forEach((sec) => {
+    sec.classList.add("hidden");
+    observer.observe(sec);
+});
+
